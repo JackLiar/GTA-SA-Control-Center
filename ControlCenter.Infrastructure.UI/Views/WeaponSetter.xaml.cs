4fe662864d68733eea8361e3baff5165f528a18b
JackWzh <zhigu1017@gmail.com>
2018-01-20 22:28:17 +0800

    add some basic user control; fill weapon status region and player status region

---
+++ ControlCenter.Infrastructure.UI/Views/WeaponSetter.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlCenter.Infrastructure.UI.Views
+{
+    /// <summary>
+    ///     Interaction logic for WeaponSetter.xaml
+    /// </summary>
+    public partial class WeaponSetter : UserControl
+    {
+        public static readonly DependencyProperty IsTextBoxEnableProperty =
+            DependencyProperty.Register("IsTextBoxEnable", typeof(bool), typeof(WeaponSetter),
+                new PropertyMetadata(true));
+
+        public WeaponSetter()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsTextBoxEnable
+        {
+            get => (bool)GetValue(IsTextBoxEnableProperty);
+            set => SetValue(IsTextBoxEnableProperty, value);
+        }
+
+        public string TextBoxText
+        {
+            get => TextBox.Text;
+            set => TextBox.Text = value;
+        }
+    }
+}

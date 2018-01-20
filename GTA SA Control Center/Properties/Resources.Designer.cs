15efce850bb57d22231e363742ca5cc078194b3f
JackWzh <zhigu1017@gmail.com>
2018-01-20 18:28:15 +0800

    Change application icon and remove abundant resources

--- GTA SA Control Center/Properties/Resources.Designer.cs
+++ GTA SA Control Center/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace ControlCenter.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon unlocked {
-            get {
-                object obj = ResourceManager.GetObject("unlocked", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }

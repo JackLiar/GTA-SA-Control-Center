4f4d6593382c2b8e8bf0715b214b3839bec69f08
JackWzh <zhigu1017@gmail.com>
2018-01-21 14:31:30 +0800

    Add Garage Module; add user controls and views

---
+++ ControlCenter.Garages/Constants/RegionNames.cs
@@ -0,0 +1,10 @@
+﻿namespace ControlCenter.Garages.Constants
+{
+    public class RegionNames
+    {
+        public const string LosSantosRegion = "LosSantosRegion";
+        public const string SanFierroRegion = "SanFierroRegion";
+        public const string LasVenturasRegion = "LasVenturasRegion";
+        public const string BoneCountyRegion = "BoneCountyRegion";
+    }
+}
---
+++ ControlCenter.Garages/Module.cs
@@ -0,0 +1,49 @@
+﻿using ControlCenter.Garages.Constants;
+using ControlCenter.Garages.Views;
+using Prism.Events;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ControlCenter.Garages
+{
+    public class Module : IModule
+    {
+        #region Constructor
+
+        public Module(IRegionManager regionManager, IEventAggregator eventAggregator)
+        {
+            _regionManager = regionManager;
+            _eventAggregator = eventAggregator;
+        }
+
+        #endregion
+
+        #region Fields & Properites
+
+        private readonly IRegionManager _regionManager;
+        private readonly IEventAggregator _eventAggregator;
+
+        #endregion
+
+        #region Methods
+
+        public void Initialize()
+        {
+            RegisterViews();
+        }
+
+        /// <summary>
+        ///     Register views with region using region manager
+        /// </summary>
+        private void RegisterViews()
+        {
+            _regionManager.RegisterViewWithRegion(Infrastructure.UI.Constants.RegionNames.GaragesRegion, typeof(Main));
+            _regionManager.RegisterViewWithRegion(RegionNames.LosSantosRegion, typeof(LosSantos));
+            _regionManager.RegisterViewWithRegion(RegionNames.SanFierroRegion, typeof(SanFierro));
+            _regionManager.RegisterViewWithRegion(RegionNames.LasVenturasRegion, typeof(LasVenturas));
+            _regionManager.RegisterViewWithRegion(RegionNames.BoneCountyRegion, typeof(BoneCounty));
+        }
+
+        #endregion
+    }
+}

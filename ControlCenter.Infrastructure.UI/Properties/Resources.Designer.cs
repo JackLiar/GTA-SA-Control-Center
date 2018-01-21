4a0e9ddf7f000594ddb9869a17ee8c0242db827b
JackWzh <zhigu1017@gmail.com>
2018-01-21 12:06:47 +0800

    Add CheatLocker user control, complete PlayerStatus region, fix picture don't show in runtime

--- ControlCenter.Infrastructure.UI/Properties/Resources.Designer.cs
+++ ControlCenter.Infrastructure.UI/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace ControlCenter.Infrastructure.UI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
+        internal static System.Drawing.Icon locked {
+            get {
+                object obj = ResourceManager.GetObject("locked", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
         internal static System.Drawing.Icon unlocked {
             get {
                 object obj = ResourceManager.GetObject("unlocked", resourceCulture);
--- ControlCenter.Infrastructure.UI/Views/CheatLocker.xaml.cs
+++ ControlCenter.Infrastructure.UI/Views/CheatLocker.xaml.cs
@@ -1,28 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ControlCenter.Infrastructure.UI.Views
 {
     /// <summary>
-    /// Interaction logic for CheatLocker.xaml
+    ///     Interaction logic for CheatLocker.xaml
     /// </summary>
     public partial class CheatLocker : UserControl
     {
+        public static readonly DependencyProperty IsLockedProperty = DependencyProperty.Register(
+            "IsLocked", typeof(bool), typeof(CheatLocker), new PropertyMetadata(default(bool)));
+
+        public static readonly DependencyProperty CheckBoxContentProperty = DependencyProperty.Register(
+            "CheckBoxContent", typeof(string), typeof(CheatLocker), new PropertyMetadata(default(string)));
+
         public CheatLocker()
         {
             InitializeComponent();
+        }
+
+        public bool IsLocked
+        {
+            get => (bool) GetValue(IsLockedProperty);
+            set => SetValue(IsLockedProperty, value);
+        }
+
+        public string CheckBoxContent
+        {
+            get => (string) GetValue(CheckBoxContentProperty);
+            set => SetValue(CheckBoxContentProperty, value);
         }
     }
 }

da4d1ea7342d72ac3e18bfcb025c69aafee2e949
JackWzh <zhigu1017@gmail.com>
2018-02-08 19:08:22 +0800

    Fix map scaling problem

--- ControlCenter.Locations/Views/Main.xaml.cs
+++ ControlCenter.Locations/Views/Main.xaml.cs
@@ -23,6 +23,31 @@ namespace ControlCenter.Locations.Views
         public Main()
         {
             InitializeComponent();
+            Loaded += SetScrollViewerToCenter;
+        }
+
+        private void SetScrollViewerToCenter(object sender, RoutedEventArgs e)
+        {
+            ScrollViewer.ScrollToHorizontalOffset((Map.ActualWidth - ScrollViewer.Width) / 2);
+            ScrollViewer.ScrollToVerticalOffset((Map.ActualHeight - ScrollViewer.Height) / 2);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var basicX = ScrollViewer.Width / 2;
+            var basicY = ScrollViewer.Width / 2;
+
+            var actualX = ScrollViewer.HorizontalOffset + basicX;
+            var actualY = ScrollViewer.VerticalOffset + basicY;
+
+            var scaledX = actualX / e.OldValue * e.NewValue;
+            var scaledY = actualY / e.OldValue * e.NewValue;
+
+            var offsetX = scaledX - basicX;
+            var offsetY = scaledY - basicY;
+
+            ScrollViewer.ScrollToHorizontalOffset(offsetX);
+            ScrollViewer.ScrollToVerticalOffset(offsetY);
         }
     }
 }
